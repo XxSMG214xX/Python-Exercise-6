469aae7bec775d96f1b26da1a67625364a90e9d3
XxSMG214xX <157718745+XxSMG214xX@users.noreply.github.com>
2024-01-27 22:19:06 +0330

    Hamkarie Roboti Clean.py

--- 4/Hamkarie Roboti.py
+++ 4/Hamkarie Roboti.py
@@ -4,9 +4,7 @@ def read_matrices(file_path) :
     with open(file_path , 'r') as file :
         data = file.readline().split()
         n , m = map(int , data)
-
         matrices = [] 
-
         for i in range (n) :
             matrix = []
             for j in range (m) :
@@ -14,9 +12,8 @@ def read_matrices(file_path) :
                 matrix.append(row)
             matrices.append(matrix)
     return matrices
-
+    
 file_path = '/Users/xxsmgxx/Desktop/code/Uni/Hamkarie Roboti/input2.txt'
-
 input = read_matrices(file_path)
 matrices = []
 for inp in input :
@@ -31,14 +28,11 @@ for i in range (len(matrices)-1) :
             first_ans = matrices[i]
             second_ans = matrices [j]
             max_dot = temp_dot
-
 if np.linalg.det(first_ans) < np.linalg.det(second_ans) :
     new_matrix = np.dot(second_ans,first_ans)
 else :
     new_matrix = np.dot(first_ans , second_ans)
-
 inverse = np.linalg.inv(new_matrix)
-
 for row in inverse:
     for element in row:
         formatted_element = "{:.3f}".format(round(element, 3))
