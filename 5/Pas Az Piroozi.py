da315c294d1834113573d800c31930ee2c96deda
XxSMG214xX <157718745+XxSMG214xX@users.noreply.github.com>
2024-01-27 22:23:57 +0330

    Clean Pas Az Piroozi.py

--- 5/Pas Az Piroozi.py
+++ 5/Pas Az Piroozi.py
@@ -2,20 +2,16 @@ def similar(n , s ,word) :
     words = s.split()
     s_w = []
     for i in words :
-        word_1 = i
-        
+        word_1 = i    
         first_word = word
-        
         if len(word) > len(i) :
             word_1 += "_" * (len(word) - len(i))
         elif len(word) < len(i) :
             first_word += "_" * (len(i) - len(word) )
-        
         d = sum (d1 != d2 for d1,d2 in zip(word_1 , first_word))
         if d <= n :
             s_w.append(i)
     return s_w
-
 n = int(input())
 s = input()
 s = s.replace(':' , '')
